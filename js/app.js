8ac646c8b47947f79ccd9c36450f4ff148f7cd0b
Morgan Jackson <contactmojack@gmail.com>
2015-08-27 16:31:14 -0500

    Added user input

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
-
-var a;	
-	for (var a = 1; a <= 100; a++ ) {
-		if (a % 5 === 0 && a % 3 === 0) {
-			document.write("Fizzbuzz<br>");
-			continue;
-		};
-		if (a % 5 === 0) {
-			document.write("Buzz<br>");
-			continue;
-		};
-		if (a % 3 === 0) {
-			document.write("Fizz<br>");
-			continue;
-		};
-		document.write(a + "<br>");
+$(document).ready(function(){
+	function fizzbuzz(max) {
+		var count = 1;	
+		for (var a = 1; a <= max; a++ ) {
+			if (a % 5 === 0 && a % 3 === 0) {
+				$("#log").append("<p>" + count + ": " + "FizzBuzz" + "</p>" );
+				count += 1;
+			}
+			else if (a % 5 === 0) {
+				$("#log").append("<p>" + count + ": " + "Buzz" + "</p>" );
+				count += 1;
+			}
+			else if (a % 3 === 0) {
+				$("#log").append("<p>" + count + ": " + "Fizz" + "</p>" );
+				count += 1;
+			}
+			else {
+				$("#log").append("<p>" + count + ": " + a + "</p>" );
+				count += 1;
+			}
+		}
 	}
+
+	function runFizzbuzz() {
+		$('input').keydown(function(enter){
+			if (enter.keycode == 13) {
+				enter.preventDefault();
+				$('p').remove();
+				var userInput = $(this).val();
+				var stringNum = parseInt(userInput, 10);
+				var num = + stringNum;
+				fizzbuzz(num);
+			}
+		})
+	}
+	runFizzbuzz();
+})
+
